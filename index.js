4d1ff0a817862d0cf3868bbaaba2154aa11b6c3a
MarkXtenda <mark.i2p.1998@gmail.com>
2022-02-18 18:50:20 +1100

    Finish

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+
+introduction("Aki");
+introduction("Samip");
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+  introductionWithLanguageOptional("Gracie", "JavaScript");
+  introductionWithLanguageOptional("Gracie", "Python");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+  introductionWithLanguageOptional("Gracie");
